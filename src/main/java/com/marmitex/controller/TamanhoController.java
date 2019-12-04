472b590444245c619c0b5650adbec082c0ccd450
usuario <juninhozatta221@gmail.com>
2019-12-04 11:35:40 -0300

    Ajustes

--- src/main/java/com/marmitex/controller/TamanhoController.java
+++ src/main/java/com/marmitex/controller/TamanhoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("marmita")
+@RequestMapping("tamanho")
 public class TamanhoController extends CrudRestController<Tamanho, Long> {
 
     private final TamanhoService tamanhoService;
