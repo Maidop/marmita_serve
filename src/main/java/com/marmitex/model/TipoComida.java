7af7c6f86b4057e87d3c8c2a20ef806a3251f12d
Victor <jvrc2000@outlook.com>
2019-12-04 15:12:18 -0300

    TipoComida already finished

---
+++ src/main/java/com/marmitex/model/TipoComida.java
@@ -0,0 +1,17 @@
+package com.marmitex.model;
+
+import javax.persistence.*;
+
+@Table(name = "COMIDA_TIPO")
+public class TipoComida {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "ID_COMIDA", nullable = false)
+    private Comida id_comida;
+
+    @Column(name = "ID_TIPO", nullable = false)
+    private Tipo id_tipo;
+}
